67055c8babe886e01d9fcbbb05fcfc154672b77c
unknown <bartoszborek00@gmail.com>
2025-03-03 14:48:53 +0100

    Create + add new user to dashboard

---
+++ React-Redux-Crud/src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "John Doe",
+        email: "profpa@code-gmail.com",
+        id: 1
+    },
+    {
+        name: "Jane Doe",
+        email: "lrgard0505@cloneads.top",
+        id: 2
+    },
+    {
+        name: "John Smith",
+        email: "ministr87@tryhivekey.com",
+        id: 3
+    }
+]
--- React-Redux-Crud/src/main.jsx
+++ React-Redux-Crud/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
+import { configureStore } from '@reduxjs/toolkit'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import UserReducer from './UserReducer'
+
+const store = configureStore({
+  reducer: {
+    users: UserReducer,
+  }
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
