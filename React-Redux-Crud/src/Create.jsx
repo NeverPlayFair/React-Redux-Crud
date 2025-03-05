8bfaf25dd259b11a333320224bb75d2e283af3b3
unknown <bartoszborek00@gmail.com>
2025-03-05 13:54:39 +0100

    Better form Validation during create method

--- React-Redux-Crud/src/Create.jsx
+++ React-Redux-Crud/src/Create.jsx
@@ -1,4 +1,3 @@
-// Create.jsx
 import { useState } from 'react';
 import { addUser } from './UserReducer';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,12 +7,40 @@ import UserForm from './UserForm';
 function Create() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    let isValid = true;
+
+    // Walidacja pola Name
+    if (name.trim() === '') {
+      setNameError('Name is required');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+
+    // Walidacja pola Email
+    if (email.trim() === '') {
+      setEmailError('Email is required');
+      isValid = false;
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      setEmailError('Invalid email address');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!isValid) return;
+
+    // Jeśli walidacja przejdzie, dodaj użytkownika
     dispatch(addUser({ id: users[users.length - 1].id + 1, name, email }));
     navigate('/');
   };
@@ -27,6 +54,8 @@ function Create() {
       email={email}
       setEmail={setEmail}
       submitLabel="Submit"
+      nameError={nameError}
+      emailError={emailError}
     />
   );
 }
--- React-Redux-Crud/src/UserForm.jsx
+++ React-Redux-Crud/src/UserForm.jsx
@@ -1,7 +1,19 @@
+
 import React from 'react';
+import PropTypes from 'prop-types';
 import { TextField, Button, Paper, Typography, Container } from '@mui/material';
 
-function UserForm({ title, onSubmit, name, setName, email, setEmail, submitLabel }) {
+function UserForm({
+  title,
+  onSubmit,
+  name,
+  setName,
+  email,
+  setEmail,
+  submitLabel,
+  nameError = '',
+  emailError = ''
+}) {
   return (
     <Container maxWidth="sm">
       <Paper elevation={3} sx={{ padding: 2, marginTop: 2 }}>
@@ -16,6 +28,8 @@ function UserForm({ title, onSubmit, name, setName, email, setEmail, submitLabel
             margin="normal"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            error={Boolean(nameError)}
+            helperText={nameError}
           />
           <TextField
             label="Email"
@@ -24,6 +38,8 @@ function UserForm({ title, onSubmit, name, setName, email, setEmail, submitLabel
             margin="normal"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            error={Boolean(emailError)}
+            helperText={emailError}
           />
           <Button
             type="submit"
@@ -39,5 +55,17 @@ function UserForm({ title, onSubmit, name, setName, email, setEmail, submitLabel
     </Container>
   );
 }
+
+UserForm.propTypes = {
+  title: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  setName: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  submitLabel: PropTypes.string.isRequired,
+  nameError: PropTypes.string,
+  emailError: PropTypes.string,
+};
 
 export default UserForm;
